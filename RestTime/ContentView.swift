fcea25d2fcf2297fa5b114c745fc8d34a1f8f400
Joey <mycityofsky@gmail.com>
2022-04-02 22:06:04 +0800

    Initial Commit

---
+++ RestTime/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RestTime
+//
+//  Created by Joey Green on 2022/4/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RestTime/RestTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestTimeApp.swift
+//  RestTime
+//
+//  Created by Joey Green on 2022/4/2.
+//
+
+import SwiftUI
+
+@main
+struct RestTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
