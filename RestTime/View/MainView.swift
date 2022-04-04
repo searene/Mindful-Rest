0e293dfb094f38cae6d8a9b72fd86593b30c99d7
Joey <mycityofsky@gmail.com>
2022-04-04 22:33:04 +0800

    Add a bottom tab bar

---
+++ RestTime/View/MainView.swift
@@ -0,0 +1,30 @@
+//
+//  MainView.swift
+//  RestTime
+//
+//  Created by Joey Green on 2022/4/4.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Label("Menu", systemImage: "list.dash")
+                }
+            
+            StatView()
+                .tabItem {
+                    Label("Statistics", systemImage: "square.and.pencil")
+                }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
---
+++ RestTime/View/StatView.swift
@@ -0,0 +1,20 @@
+//
+//  StatView.swift
+//  RestTime
+//
+//  Created by Joey Green on 2022/4/4.
+//
+
+import SwiftUI
+
+struct StatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StatView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatView()
+    }
+}
