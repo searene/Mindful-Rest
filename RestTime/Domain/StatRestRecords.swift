59ba1c3d28b320edd7ca5163c1f14fbb75742e40
Joey <mycityofsky@gmail.com>
2022-04-16 10:06:19 +0800

    add useRestRecords.

--- RestTime/Domain/StatRestRecords.swift
+++ RestTime/Domain/StatRestRecords.swift
@@ -9,4 +9,13 @@ import Foundation
 
 class StatRestRecords: ObservableObject {
     @Published var restRecords: [RestRecord] = []
+    
+    @Published var proportions: [Float] = []
+    
+    func useRestRecords(restRecords: [RestRecord]) -> Void {
+        self.restRecords = restRecords
+        self.proportions = restRecords.enumerated().map { index, record in
+            RestRecord.getDurationProportion(restRecords: restRecords, targetIndex: index)
+        }
+    }
 }
